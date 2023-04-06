2cbe26ec6414ce2728508c98185fd33a064fa94d
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-04-06 14:35:03 -0500

    adds solution to 6 and 7

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -84,7 +84,7 @@ else {
 ////////// PROBLEM 6 //////////
 
 // Do not edit the code below.
-var myFavoriteNumbers = [4,8,12,16,20,24];
+var myFavoriteNumbers = [4,8,12,16,20,24,7];
 // Do not edit the code above.
 
 /*
@@ -92,15 +92,25 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
-
+let someNum = myFavoriteNumbers[4]
 
 ////////// PROBLEM 7 //////////
 
-// Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
-// Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
+// // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element,
+//  but there are not 7 elements in the array? Let's write some code to check for that. 
+// // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 
+// 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 
+// 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? 
+//   What index do you use to get the 7th element?)
 
 //Code Here
-
+if (myFavoriteNumbers.length < 7){
+  console.log("There are not enough elements in this array")
+}
+else if(myFavoriteNumbers.length >= 7){
+  someNum = myFavoriteNumbers[6]
+}
+console.log("Number",someNum)
 
 ////////// PROBLEM 8 //////////
 
